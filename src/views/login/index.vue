dc93c649cda8c624d16c207ba842f605ad6cd17f
周鑫磊 <charles.zhou@farben.com.cn>
2021-03-19 18:26:44 +0800

    代码优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,59 +1,82 @@
 <template>
-  <div
-    id="dowebok"
-    :class="activeLogin !== true ? 'dowebok' : 'dowebok right-panel-active'"
-  >
+  <div :class="activeLogin === true ? 'dowebok' : 'dowebok right-panel-active'">
     <div class="form-container sign-up-container">
-      <form action="#">
-        <div>
+      <el-form ref="ruleForm" :model="form" size="medium">
+        <div style="line-height: 40px">
           <img style="width: 40px; height: 40px" src="@/assets/logo.png" />
           <span style="font-size: 20px; font-weight: bold">
             欢迎加入Spark Auth
           </span>
+          <br />
+          <span>填写个人信息</span>
         </div>
-        <span>填写个人信息</span>
-        <input id="tenant" type="text" name="tenant" placeholder="领域池编码" />
-        <input id="account" type="text" name="account" placeholder="账号" />
-        <input type="email" placeholder="电子邮箱" />
-        <input type="password" placeholder="密码" />
-        <button>注册</button>
-      </form>
+        <el-form-item prop="label" required>
+          <el-input
+            v-model="form.username"
+            placeholder="请输入账户或者手机号"
+            class="edit-form-item"
+          />
+        </el-form-item>
+        <el-form-item required>
+          <el-input
+            v-model="form.password"
+            type="password"
+            placeholder="请输入邮箱"
+            class="edit-form-item"
+          />
+        </el-form-item>
+        <el-form-item required>
+          <el-input
+            v-model="form.password"
+            type="password"
+            placeholder="请输入密码"
+            class="edit-form-item"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button size="small" type="primary">注 册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <div class="form-container sign-in-container">
-      <form action="/authentication/form" method="post">
-        <div>
+      <el-form ref="ruleForm" :model="form" size="medium">
+        <div style="line-height: 40px">
           <img style="width: 40px; height: 40px" src="@/assets/logo.png" />
           <span style="font-size: 20px; font-weight: bold">Spark Auth</span>
+          <br />
+          <span>使用帐号登录</span>
         </div>
-        <span>使用帐号登录</span>
-        <input
-          id="tenantCode"
-          type="text"
-          name="tenantCode"
-          placeholder="领域池编码"
-        />
-        <input id="username" type="text" name="username" placeholder="账户" />
-        <input
-          id="password"
-          type="password"
-          name="password"
-          placeholder="密码"
-        />
-        <a href="#">忘记密码？</a>
-        <button id="login" type="submit">登录</button>
-      </form>
+        <el-form-item prop="label" required>
+          <el-input
+            v-model="form.username"
+            placeholder="请输入账户或者手机号"
+            class="edit-form-item"
+          />
+        </el-form-item>
+        <el-form-item required>
+          <el-input
+            v-model="form.password"
+            type="password"
+            placeholder="请输入密码"
+            class="edit-form-item"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-button size="small" type="primary">登 录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <div class="overlay-container">
       <div class="overlay">
         <div class="overlay-panel overlay-left">
           <h1>已有帐号？</h1>
           <p>请使用您的帐号进行登录</p>
-          <button id="signIn" class="ghost">登录</button>
+          <button id="signIn" class="ghost" @click="changeLogin">登录</button>
         </div>
         <div class="overlay-panel overlay-right">
           <h1>没有帐号？</h1>
           <p>立即注册加入我们，和我们一起开始旅程吧</p>
-          <el-button @click="changeRegister">注册</el-button>
+          <button class="ghost" @click="changeRegister">注册</button>
         </div>
       </div>
     </div>
@@ -64,11 +87,18 @@
   export default {
     data() {
       return {
+        form: {
+          username: null,
+          password: null,
+        },
         activeLogin: true,
       }
     },
     methods: {
       changeRegister() {
+        this.activeLogin = false
+      },
+      changeLogin() {
         this.activeLogin = true
       },
     },
@@ -173,8 +203,8 @@
 
   button {
     border-radius: 20px;
-    border: 1px solid $base-color-default;
-    background: $base-color-default;
+    border: 1px solid $base-login-color;
+    background: $base-login-color;
     color: #fff;
     font-size: 12px;
     font-weight: bold;
@@ -301,147 +331,5 @@
   /* Same effect for right */
   .dowebok.right-panel-active .overlay-right {
     transform: translateY(20%);
-  }
-</style>
-<script></script>
-<style lang="scss" scoped>
-  .login-container {
-    height: 100vh;
-    background: url('~@/assets/login_images/background.jpg') center center fixed
-      no-repeat;
-    background-size: cover;
-
-    .title {
-      font-size: 54px;
-      font-weight: 500;
-      color: rgba(14, 18, 26, 1);
-    }
-
-    .title-tips {
-      margin-top: 29px;
-      font-size: 26px;
-      font-weight: 400;
-      color: rgba(14, 18, 26, 1);
-      text-overflow: ellipsis;
-      white-space: nowrap;
-    }
-
-    .login-btn {
-      display: inherit;
-      width: 220px;
-      height: 60px;
-      margin-top: 5px;
-      border: 0;
-
-      &:hover {
-        opacity: 0.9;
-      }
-    }
-
-    .login-form {
-      position: relative;
-      max-width: 100%;
-      margin: calc((100vh - 425px) / 3) 10% 10%;
-      overflow: hidden;
-
-      .forget-password {
-        width: 100%;
-        margin-top: 40px;
-        text-align: left;
-
-        .forget-pass {
-          width: 129px;
-          height: 19px;
-          font-size: 20px;
-          font-weight: 400;
-          color: rgba(92, 102, 240, 1);
-        }
-      }
-    }
-
-    .tips {
-      margin-bottom: 10px;
-      font-size: $base-font-size-default;
-      color: $base-color-white;
-
-      span {
-        &:first-of-type {
-          margin-right: 16px;
-        }
-      }
-    }
-
-    .title-container {
-      position: relative;
-
-      .title {
-        margin: 0 auto 40px auto;
-        font-size: 34px;
-        font-weight: bold;
-        color: $base-color-blue;
-        text-align: center;
-      }
-    }
-
-    .svg-container {
-      position: absolute;
-      top: 14px;
-      left: 15px;
-      z-index: $base-z-index;
-      font-size: 16px;
-      color: #d7dee3;
-      cursor: pointer;
-      user-select: none;
-    }
-
-    .show-password {
-      position: absolute;
-      top: 14px;
-      right: 25px;
-      font-size: 16px;
-      color: #d7dee3;
-      cursor: pointer;
-      user-select: none;
-    }
-
-    ::v-deep {
-      .el-form-item {
-        padding-right: 0;
-        margin: 20px 0;
-        color: #454545;
-        background: transparent;
-        border: 1px solid transparent;
-        border-radius: 2px;
-
-        &__content {
-          min-height: $base-input-height;
-          line-height: $base-input-height;
-        }
-
-        &__error {
-          position: absolute;
-          top: 100%;
-          left: 18px;
-          font-size: $base-font-size-small;
-          line-height: 18px;
-          color: $base-color-red;
-        }
-      }
-
-      .el-input {
-        box-sizing: border-box;
-
-        input {
-          height: 58px;
-          padding-left: 45px;
-          font-size: $base-font-size-default;
-          line-height: 58px;
-          color: $base-font-color;
-          background: #f6f4fc;
-          border: 0;
-          caret-color: $base-font-color;
-        }
-      }
-    }
   }
 </style>
