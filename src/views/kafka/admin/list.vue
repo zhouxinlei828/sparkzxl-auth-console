fda152e14611fb442ad3a2b84891b9294897ac63
周鑫磊 <charles.zhou@farben.com.cn>
2021-03-29 16:31:06 +0800

    代码优化

---
+++ src/views/kafka/admin/list.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <KafkaFrame :url="url" />
+  </div>
+</template>
+<script>
+  import KafkaFrame from '../modules/kafkaFrame'
+
+  export default {
+    components: {
+      KafkaFrame,
+    },
+    data() {
+      return {
+        url: `${process.env.VUE_APP_KAFKA_IFRAME_URL}/kafka/admin`,
+      }
+    },
+    methods: {},
+  }
+</script>
---
+++ src/views/kafka/modules/kafkaFrame.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Iframe
+      id="iframe"
+      frameborder="no"
+      width="100%"
+      style="overflow-y: auto"
+      height="500"
+      :src="urlNormal"
+    ></Iframe>
+  </div>
+</template>
+<script>
+  import { getAccessToken } from '@/utils/storageUtils'
+  export default {
+    components: {},
+    props: {
+      // eslint-disable-next-line vue/require-default-prop
+      url: String,
+    },
+    data() {
+      return {}
+    },
+    computed: {
+      urlNormal(state) {
+        return `${state.url}?token=Bearer `.concat(getAccessToken())
+      },
+    },
+    mounted() {},
+    methods: {},
+  }
+</script>
