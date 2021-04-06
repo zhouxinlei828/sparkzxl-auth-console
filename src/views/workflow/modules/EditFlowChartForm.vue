4cb2b9e273dad1c6e6b175e0943cf558dbd2308f
周鑫磊 <charles.zhou@farben.com.cn>
2021-04-06 14:29:09 +0800

    代码优化

--- src/views/workflow/modules/EditFlowChartForm.vue
+++ src/views/workflow/modules/EditFlowChartForm.vue
@@ -31,7 +31,6 @@
     data() {
       return {
         dialogFormVisible: false,
-        form: this.$form.createForm(this),
         flowChartSrc: '',
         getWindowInfo: {
           height: '',
